9261ab145e510c3803433f8fa998e1f2bee79977
durgeshwari <durgeshwarinaik@gmail.com>
2020-12-19 16:37:12 +0530

    pyPredict update

--- personality classification/pyPredict.py
+++ personality classification/pyPredict.py
@@ -1,4 +1,4 @@
-#  -*- coding: utf-8 -*-
+
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import *
@@ -18,11 +18,19 @@ import pickle
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from collections import Counter
+import nltk
+nltk.download('stopwords')
+import nltk
+nltk.download('punkt')
+
+print("Hello")
+
 
 ckey='YvQZ4wcEhYo4F4Lj8PQqj500d'
 csecret='nDMvzbvQxt2zW9GZOalnOikeDcwGwPwvi2USOD5phJCmw5e9wD'
 atoken='994417184322433025-anaAEWzZErK5h8CGTxEgJMPFwfRoMNA'
 asecret='M2RwOR7CaWHMO7d83iWuKhHmEtgyaomobBNt9bsJhSF3i'
+
 auth=tweepy.OAuthHandler(ckey, csecret)
 auth.set_access_token(atoken, asecret)
 api=tweepy.API(auth)
@@ -68,6 +76,7 @@ def preprocess(s, lowercase=False):
     if lowercase:
         tokens = [token if emoticon_re.search(token) else token.lower() for token in tokens]
     return tokens
+
 def preproc(s):
 	#s=emoji_pattern.sub(r'', s) # no emoji
 	s= unidecode(s)
@@ -78,14 +87,18 @@ def preproc(s):
 	stop_words = set(stopwords.words('english'))
 	word_tokens = word_tokenize(tweet)
 	#filtered_sentence = [w for w in word_tokens if not w in stop_words]
+    
 	filtered_sentence = []
 	for w in POSTagger:
 	    if w not in stop_words:
 	        filtered_sentence.append(w)
+            
 	#print(word_tokens)
 	#print(filtered_sentence)
+    
 	stemmed_sentence=[]
 	stemmer2 = SnowballStemmer("english", ignore_stopwords=True)
+    
 	for w in filtered_sentence:
 		stemmed_sentence.append(stemmer2.stem(w))
 	#print(stemmed_sentence)
@@ -93,6 +106,7 @@ def preproc(s):
 	temp = ' '.join(c for c in stemmed_sentence if c not in string.punctuation) 
 	preProcessed=temp.split(" ")
 	final=[]
+    
 	for i in preProcessed:
 		if i not in final:
 			if i.isdigit():
@@ -123,10 +137,12 @@ def getTweets(user):
 
 username=input("Please Enter Twitter Account handle: ")
 getTweets(username)
+
 with open('user.csv','rt') as f:
 	csvReader=csv.reader(f)
 	tweetList=[rows[0] for rows in csvReader]
 os.remove('user.csv')
+
 with open('CSV_Data/newfrequency300.csv','rt') as f:
 	csvReader=csv.reader(f)
 	mydict={rows[1]: int(rows[0]) for rows in csvReader}
@@ -147,9 +163,18 @@ SN=model_SN.predict(df)
 TF=model_TF.predict(df)
 PJ=model_PJ.predict(df)
 
+pred_perc=[]
 
 b = Counter(IE)
+
 value=b.most_common(1)
+
+
+b=dict(b)
+pred_perc.append((value[0][1]*100)//(b[1.0]+b[0.0]))
+print('value',value)
+
+
 #print(value)
 if value[0][0] == 1.0:
 	answer.append("I")
@@ -157,8 +182,15 @@ else:
 	answer.append("E")
 
 b = Counter(SN)
+
 value=b.most_common(1)
 #print(value)
+
+
+b=dict(b)
+pred_perc.append((value[0][1]*100)//(b[1.0]+b[0.0]))
+
+
 if value[0][0] == 1.0:
 	answer.append("S")
 else:
@@ -167,6 +199,12 @@ else:
 b = Counter(TF)
 value=b.most_common(1)
 #print(value)
+
+
+b=dict(b)
+pred_perc.append((value[0][1]*100)//(b[1.0]+b[0.0]))
+
+
 if value[0][0] == 1:
 	answer.append("T")
 else:
@@ -175,112 +213,154 @@ else:
 b = Counter(PJ)
 value=b.most_common(1)
 #print(value)
+
+b=dict(b)
+pred_perc.append((value[0][1]*100)//(b[1.0]+b[0.0]))
+
 if value[0][0] == 1:
 	answer.append("P")
 else:
 	answer.append("J")
 mbti="".join(answer)
+
 # Classifying Personality's ==========================================>
+
 print('===============================================================================>')
 
 print('User Name - ' + username + '\n')
 
 print('Type of Personality \n')
 
+
+char_per=dict()
+opp_char ={'I':'E','E':'I','S':'N','N':'S',
+'T':'F','F':'T','J':'P','P':'J'}
+for i in range(4):
+    char_per[answer[i]]=pred_perc[i]
+    char_per[opp_char[answer[i]]]=100-pred_perc[i]
+#print(char_per)
+
+all_pers = ['ENFJ','ISTJ','INFJ','INTJ','INTP','ESFJ','INFP','ENFP','ESTP','ESTJ','ENTJ']
+pers_perc={}
+for word in all_pers:
+    perc=sum([char_per[el] for el in word])//4
+    pers_perc[word]=perc
+print(pers_perc)
+
+
+
 if mbti == 'ENFJ':
 	str1 = '" The Giver "'
 	print(mbti +' - '+ str1)
 	print("\nThe Giver :-")
-	print(" They are extroverted, idealistic, charismatic, outspoken, \n highly principled and ethical, and usually know how to connect \n with others no matter their background or personality. ")
+	print(" A Protagonist (ENFJ) is a person with the Extraverted, Intuitive, Feeling,\n and Judging personality traits. These warm, forthright types love \n helping others, and they tend to have strong ideas and values. They back their perspective \n with the creative energy to achieve their goals.")
+	print("Famous personalities whose traits you resemble are: \n Barack Obama,Oprah Winfrey , John Cusack, Ben Affleck, Malala Yousafzai.")
+
 
 elif mbti == 'ISTJ':
 	str1 = '" The Inspector "'
 	print(mbti +' - '+ str1)
 	print("\nThe Inspector :-")
-	print(" They appear serious, formal, and proper. They also love \n traditions and old-school values that uphold patience, hard work,\n honor, and social and cultural responsibility. They are reserved, calm, quiet, and upright. ")
+	print("A Logistician (ISTJ) is someone with the Introverted, Observant,\n Thinking, and Judging personality traits. These people tend to be reserved yet willful,\n with a rational outlook on life. They compose their actions carefully and carry \n them out with methodical purpose.")
+	print("Famous personalities whose traits you resemble are: \n George H.W. Bush, Hermione Granger(Harry Potter), George Washington.")
 
 elif mbti == 'INFJ':
 	str1 = '" The Counselor "'
 	print(mbti +' - '+ str1)
 	print("\nThe Counselor :-")
-	print(" They have a different, and usually more profound, way of\n looking at the world. They have a substance and depth in the way \n they think, never taking anything at surface level or accepting things the way they are")
+	print(" An Advocate (INFJ) is someone with the Introverted, Intuitive, \n Feeling, and Judging personality traits. They tend to approach life with \n deep thoughtfulness and imagination. Their inner vision, personal values, \n and a quiet, principled version of humanism guide them in all things.")
+	print("Famous personalities whose traits you resemble are: \n Nelson Mandela, Mother Teresa, Martin Luther King.")
 
 elif mbti == 'INTJ':
 	str1 = '" The Mastermind "'
 	print(mbti +' - '+ str1)
 	print("\nThe Mastermind :-")
-	print(" They are usually self-sufficient and would rather work \n alone than in a group. Socializing drains an introvert’s energy, \n causing them to need to recharge.")
+	print(" An Architect (INTJ) is a person with the Introverted, Intuitive, Thinking,\n and Judging personality traits. \n These thoughtful tacticians love perfecting the details of life, applying creativity and \n rationality to everything they do.\n Their inner world is often a private, complex one.")
+	print("Famous personalities whose traits you resemble are: \n Miclelle Obama, Elon Musk, Christopher Nolan, Friedrich Nietzsche.")
 
 elif mbti == 'ISTP':
 	str1 = '" The Craftsman "'
 	print(mbti +' - '+ str1)
 	print("\nThe Craftsman :-")
-	print(" They are mysterious people who are usually very rational\n and logical, but also quite spontaneous and enthusiastic. Their \n personality traits are less easily recognizable than those of other \n types, and even people who know them well can’t always anticipate \n their reactions. Deep down, ISTPs are spontaneous, unpredictable individuals,\n but they hide those traits from the outside world, often very successfully.")
+	print(" A Virtuoso (ISTP) is someone with the Introverted, Observant, \n Thinking, and Prospecting personality traits. They tend to have an individualistic \n mindset, pursuing goals without needing much external connection. They engage in \n life with inquisitiveness and personal skill, varying their approach as needed.")
+	print("Famous personalities whose traits you resemble are: \n Bear Grylls, Olivia Wilde, Tom Cruise .")
 
 elif mbti == 'ESFJ':
 	str1 = '" The Provider "'
 	print(mbti +' - '+ str1)
 	print("\nThe Provider :-")
-	print(" They are social butterflies, and their need to interact\n with others and make people happy usually ends up making them popular.\n The ESFJ usually tends to be the cheerleader or sports hero in high school\n and college. Later on in life, they continue to revel in the \n spotlight, and are primarily focused on organizing social events for their families,\n friends and communities. ")
+	print(" A Consul (ESFJ) is a person with the Extraverted, Observant, \n Feeling, and Judging personality traits. They are attentive and people-focused,\n and they enjoy taking part in their social community. Their achievements are guided \n by decisive values, and they willingly offer guidance to others. ")
+	print("Famous personalities whose traits you resemble are: \n Bill Clinton,Taylor Swift,Steve Harvey.")
 
 elif mbti == 'INFP':
 	str1 = '" The Idealist "'
 	print(mbti +' - '+ str1)
 	print("\nThe Idealist :-")
-	print(" They prefer not to talk about themselves, especially in \n the first encounter with a new person. They like spending time alone in \n quiet places where they can make sense of what is happening around them. \n They love analyzing signs and symbols, and consider them to be \n metaphors that have deeper meanings related to life. ")
+	print("  A Mediator (INFP) is someone who possesses the Introverted, \n Intuitive, Feeling, and Prospecting personality traits. These rare personality \n types tend to be quiet, open-minded, and imaginative, and they apply a caring and \n creative approach to everything they do. ")
+	print("Famous personalities whose traits you resemble are: \n William Shakespeare,To Hiddleston, William Wordsworth.")
 
 elif mbti == 'ESFP':
 	str1 = '" The Performer "'
 	print(mbti +' - '+ str1)
 	print("\nThe Performer :-")
-	print(" They have an Extraverted, Observant, Feeling and Perceiving \n personality, and are commonly seen as Entertainers. Born to be in \n front of others and to capture the stage, ESFPs love the spotlight. ESFPs \n are thoughtful explorers who love learning and sharing what they \n learn with others. ESFPs are “people people” with strong interpersonal skills.")
+	print("An Entertainer (ESFP) is a person with the Extraverted, Observant, Feeling, and Prospecting personality traits. These people love vibrant experiences, engaging in life eagerly and taking pleasure in discovering the unknown. They can be very social, often encouraging others into shared activities.")
+	print("Famous personalities whose traits you resemble are: \n Adele, Elton John, Jamie Foxx ")
 
 elif mbti == 'ENFP':
 	str1 = '" The Champion "'
 	print(mbti +' - '+ str1)
 	print("\nThe Champion :-")
-	print(" They have an Extraverted, Intuitive, Feeling and Perceiving \n personality. This personality type is highly individualistic and \n Champions strive toward creating their own methods, looks, actions, habits, \n and ideas — they do not like cookie cutter people and hate when they \n are forced to live inside a box. ")
+	print(" A Campaigner (ENFP) is someone with the Extraverted, Intuitive, \n Feeling, and Prospecting personality traits. These people tend to embrace big ideas \n and actions that reflect their sense of hope and goodwill toward others. Their vibrant \n energy can flow in many directions. ")
+	print("Famous personalities whose traits you resemble are: \n Will Smith, Kelly Clarkson, Robert Downey.")
 
 elif mbti == 'ESTP':
 	str1 = '" The Doer "'
 	print(mbti +' - '+ str1)
 	print("\nThe Doer :-")
-	print(" They have an Extraverted, Sensing, Thinking, and Perceptive\n personality. ESTPs are governed by the need for social interaction,\n feelings and emotions, logical processes and reasoning, along with a need \n for freedom. ")
+	print(" An Entrepreneur (ESTP) is someone with the Extraverted, Observant, \n Thinking, and Prospecting personality traits. They tend to be energetic and \n action-oriented, deftly navigating whatever is in front of them. They love \n uncovering life’s opportunities, whether socializing with others or in more personal pursuits. ")
+	print("Famous personalities whose traits you resemble are: \n Jack Nicholson, Ernest Hemingway,Nicolas Sarkozy.")
 
 elif mbti == 'ESTJ':
 	str1 = '" The Supervisor "'
 	print(mbti +' - '+ str1)
 	print("\nThe Supervisor :-")
-	print(" They are organized, honest, dedicated, dignified, traditional,\n and are great believers of doing what they believe is right and \n socially acceptable. Though the paths towards “good” and “right” are difficult,\n they are glad to take their place as the leaders of the pack. \n They are the epitome of good citizenry. ")
+	print(" An Executive (ESTJ) is someone with the Extraverted, Observant, \n Thinking, and Judging personality traits. They possess great fortitude, \n emphatically following their own sensible judgment. They often serve as a \n stabilizing force among others, able to offer solid direction amid adversity.")
+	print("Famous personalities whose traits you resemble are: \n John D Rockefeller, James Monroe , Laura linney, Sonia Sotomayor.")
 
 elif mbti == 'ENTJ':
 	str1 = '" The Commander "'
 	print(mbti +' - '+ str1)
 	print("\nThe Commander :-")
 	print(" Their secondary mode of operation is internal, where intuition \n and reasoning take effect. ENTJs are natural born leaders among \n the 16 personality types and like being in charge. They live in a world of \n possibilities and they often see challenges and obstacles as great \n opportunities to push themselves.")
+	print("Famous personalities whose traits you resemble are: \n Steve jobs, Franklin D. Roosevelt, Gordon Ramsay.")
 
 elif mbti == 'INTP':
 	str1 = '" The Thinker "'
 	print(mbti +' - '+ str1)
 	print("\nThe Thinker :-")
-	print(" They are well known for their brilliant theories and unrelenting\n logic, which makes sense since they are arguably the most logical \n minded of all the personality types. They love patterns, have a keen eye \n for picking up on discrepancies, and a good ability to read people, \n making it a bad idea to lie to an INTP.")
+	print(" A Logician (INTP) is someone with the Introverted, Intuitive, Thinking, \n and Prospecting personality traits. These flexible thinkers enjoy taking \n an unconventional approach to many aspects of life. They often seek out \n unlikely paths, mixing willingness to experiment with personal creativity.")
+	print("Famous personalities whose traits you resemble are: \n Albert Einstein, Bill Gates, Issac Newton, Blaise Pascal.")
 
 elif mbti == 'ISFJ':
 	str1 = '" The Nurturer "'
 	print(mbti +' - '+ str1)
 	print("\nThe Nurturer :-")
-	print(" They are philanthropists and they are always ready to give back \n and return generosity with even more generosity. The people \n and things they believe in will be upheld and supported with enthusiasm\n  and unselfishness.")
+	print(" A Defender (ISFJ) is someone with the Introverted, Observant,\n Feeling, and Judging personality traits. These people tend to be warm and \n unassuming in their own steady way. They’re efficient and responsible, giving \n careful attention to practical details in their daily lives.")
+	print("Famous personalities whose traits you resemble are: \n Queen Elizabeth II, Aretha Franklin, Vin Diesel.")
 
 elif mbti == 'ENTP':
 	str1 = '" The Visionary "'
 	print(mbti +' - '+ str1)
 	print("\nThe Visionary :-")
-	print(" Those with the ENTP personality are some of the rarest in the world,\n  which is completely understandable. Although they are \n extroverts, they don’t enjoy small talk and may not thrive in many social \n situations, especially those that involve people who are too different\n from the ENTP. ENTPs are intelligent and knowledgeable need to \n be constantly mentally stimulated. ")
+	print(" A Debater (ENTP) is a person with the Extraverted, Intuitive, \n Thinking, and Prospecting personality traits. They tend to be bold and creative,\n  deconstructing and rebuilding ideas with great mental agility. They pursue \n their goals vigorously despite any resistance they might encounter.. ")
+	print("Famous personalities whose traits you resemble are: \n Thomas Edison, Mark Twain, Tom Hanks .")
 
 else :
 	str1 = '" The Composer "'
 	print(mbti +' - '+ str1)
 	print("\nThe Composer :-")
-	print(" They are introverts that do not seem like introverts. It is \n because even if they have difficulties connecting to other people\n at first, they become warm, approachable, and friendly eventually. They \n are fun to be with and very spontaneous, which makes them the perfect \n friend to tag along in whatever activity, regardless if planned \n or unplanned. ISFPs want to live their life to the fullest and embrace the\n present, so they make sure they are always out to explore new things and \n discover new experiences.")
+	print(" An Adventurer (ISFP) is a person with the Introverted, Observant, \n Feeling, and Prospecting personality traits. They tend to have open minds,\n approaching life, new experiences, and people with grounded warmth. \n Their ability to stay in the moment helps them uncover exciting potentials.")
+	print("Famous personalities whose traits you resemble are: \n Michael Jackson, Jeon jungkook,Frida Kahlo .")
 
 print('===============================================================================>')
+
